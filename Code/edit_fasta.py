fbed045909ab530509db23a91e54849986364c58
hukuhuku <hukuhuku11111arm52@gmail.com>
2018-07-20 11:18:40 +0900

    [add]コメントを追加

--- Code/edit_fasta.py
+++ Code/edit_fasta.py
@@ -3,6 +3,14 @@ import sys
 import re
 
 def match_extraction(description,s_match,pre=1):
+    """
+    description :str
+    s_match :str
+    pre : 1 or 0 
+    -------------
+    descriptionの文字列内にs_matchがあればマッチした部分の前後を取り出す
+    """
+
     try:
         r = re.search(s_match,description)
         if pre == 1:
@@ -13,9 +21,13 @@ def match_extraction(description,s_match,pre=1):
         return description
 
 def main():
+    """
+    fasta_inに渡したファイルを何らかの処理をしてoutputのファイルに渡す
+    """
+    
     fasta_in = sys.argv[1]
     fasta_out = "../output/result.fasta"
-    match_str = "tria"
+    match_str = ""
 
     file = open(fasta_out,"w")
     for record in SeqIO.parse(fasta_in, 'fasta'):
