10fc532cf1f4c46527d63173a6386bc7bca6b3e4
hukuhuku <hukuhuku11111arm52@gmail.com>
2018-07-20 11:31:26 +0900

    [add]not_re_match関数を追加

--- Code/edit_fasta.py
+++ Code/edit_fasta.py
@@ -2,29 +2,42 @@ from Bio import SeqIO
 import sys
 import re
 
-def match_extraction(description,s_match,pre=1):
+def match_extraction(description,s_match,pre=True):
     """
     description :str
     s_match :str
-    pre : 1 or 0 
+    pre : True or False
     -------------
     descriptionの文字列内にs_matchがあればマッチした部分の前後を取り出す
     """
 
     try:
         r = re.search(s_match,description)
-        if pre == 1:
+        if pre:
             return r.string[:r.start()]
         else:
             return r.string[r.end():]
     except:
         return description
 
+def not_re_match(description,s_match,pre = True):
+    """
+    正規表現を使わないバージョン
+    """
+    try:
+        if pre:
+            return description.split(s_match)[0]
+        else:
+            return description.split(s_match)[1]
+    except:
+        return description   
+
+
 def main():
     """
     fasta_inに渡したファイルを何らかの処理をしてoutputのファイルに渡す
     """
-    
+
     fasta_in = sys.argv[1]
     fasta_out = "../output/result.fasta"
     match_str = ""
@@ -34,6 +47,7 @@ def main():
         seq_part = record.seq 
         desc_part = record.description
         
+        #fileに書き出す前に文字列を処理する関数追加する。
         desc_part = match_extraction(desc_part,match_str)
 
         file.write(">"+desc_part+"\n")
